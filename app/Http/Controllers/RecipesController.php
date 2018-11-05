dce571229958a6ef811bb3aff5cf0a7f5a4e2be4
Checho <sergio.avila.astorga@gmail.com>
2018-11-05 00:08:09 -0300

    recipes eloquent methods

--- app/Http/Controllers/RecipesController.php
+++ app/Http/Controllers/RecipesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\Recipe;
 use App\Models\RecipeIngredient;
+use App\Models\Category;
 use App\Models\Ingredient;
 use App\Models\Image;
 
@@ -11,21 +12,32 @@ class RecipesController extends Controller
     //Get All recipes
     function index()
     {
-      return response()->json(Recipe::all());
+      $recipes = Recipe::with('ingredients')->get();
+      $full_recipes = [];
+      foreach ($recipes as $recipe) {
+        $image = Image::select('url')->where('id', $recipe['image_id'])->first();
+        $category = Category::select('name')->where('id', $recipe['category_id'])->first();
+        $full_recipes[] = ['name'=>$recipe->name, 'description'=>$recipe->description,'image'=>$image->url,'category'=>$category->name];
+      }
+      return response()->json($full_recipes);
     }
     //Single recipe
     function single($id)
     {
-      $recipe = Recipe::find($id);
-      $ingredients = $recipe->ingredients;
-      $category = $recipe->category;
-      $image = $recipe->image;
-      return response()->json($ingredients);
+      $recipe = Recipe::select('name','description','category_id','image_id')->where('id', $id)->first();
+      $ingredients = Recipe::find($id)->ingredients->pluck('name');
+      $category = Category::select('name')->where('id', $recipe['category_id'])->first();
+      $image = Image::select('url')->where('id', $recipe['image_id'])->first();
+
+      $full_recipe = collect(['name'=>$recipe->name, 'description'=>$recipe->description,['ingredients'=>$ingredients],'image'=>$image->url,'category'=>$category->name]);
+
+      return response()->json($full_recipe);
     }
     //Search recipe
     function search($terms = 'amer')
     {
       $recipe = Recipe::where('name', 'LIKE' ,'%'.$terms.'%')->get();
+
       return response()->json($recipe);
     }
 
